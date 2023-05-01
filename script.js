7c7a8321a3d0dda8c0056980a9e2dc3cc2ecf30f
istekhar khan <istekharkhan46@gmail.com>
2023-05-02 01:24:03 +0530

    made little bit responsive

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ setInterval (()=>{
             let d = new Date();
 timer.innerHTML = "Sale Ends in  "+ d.getHours() +"<p>H</p> : " +d.getMinutes()+"<p>M</p> : "+d.getSeconds()+ "<p>s</p>" ;
 },1000)
+
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
+
+const toggleNavbar = () =>{
+    nav_header.classList.toggle("active");
+}
+
+mobile_nav.addEventListener('click',()=>toggleNavbar());
