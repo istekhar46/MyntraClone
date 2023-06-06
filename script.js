56cc1e24d3d224233aa0c63a96bda7651af08a07
istekhar khan <istekharkhan46@gmail.com>
2023-06-06 16:04:12 +0530

    fixed timer of sale

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 let timer = document.getElementsByClassName("deal-timer")[0];
-setInterval (()=>{
-            let d = new Date();
-timer.innerHTML = "Sale Ends in  "+ d.getHours() +"<p>H</p> : " +d.getMinutes()+"<p>M</p> : "+d.getSeconds()+ "<p>s</p>" ;
-},1000)
+setInterval(() => {
+  let d = new Date();
+  let hours = 23 - d.getHours();
+  let minutes = 59 - d.getMinutes();
+  let seconds = 59 - d.getSeconds();
+
+  timer.innerHTML =
+    "Sale Ends in " +
+    hours +
+    "<p>H</p> : " +
+    minutes +
+    "<p>M</p> : " +
+    seconds +
+    "<p>s</p>";
+}, 1000);
 
 const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
 
-const toggleNavbar = () =>{
-    nav_header.classList.toggle("active");
-}
+const toggleNavbar = () => {
+  nav_header.classList.toggle("active");
+};
 
-mobile_nav.addEventListener('click',()=>toggleNavbar());
+mobile_nav.addEventListener("click", () => toggleNavbar());
